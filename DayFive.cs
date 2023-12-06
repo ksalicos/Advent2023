403a5971956b45f20f023687bb7d462ef85a4cc1
Kevin Salicos <ksalicos@gmail.com>
2023-12-05 16:39:53 -0800

    Day Five - Part Two

--- DayFive.cs
+++ DayFive.cs
@@ -29,7 +29,7 @@ namespace Advent2023
                 {
                     foreach (var k in _seedMap.Keys)
                     {
-                        _seedMap[k] = SearchMaps(_seedMap[k], mk);
+                        _seedMap[k] = MapValue(_seedMap[k], _maps[mk]);
                     }
                 }
 
@@ -62,7 +62,7 @@ namespace Advent2023
                 sr.Close();
 
                 PartOne();
-                // PartTwo();
+                PartTwo();
             }
             catch (Exception e)
             {
@@ -90,39 +90,109 @@ namespace Advent2023
             return result;
         }
 
-        private long SearchMaps(long k, string m)
+        private long MapValue(long k, List<Map> maps)
         {
-            var map = _maps[m].FirstOrDefault(x => k >= x.Key && k <= x.Key + x.Length);
+            var map = maps.FirstOrDefault(x => k >= x.Source && k <= x.Source + x.Length);
             if (map == null) return k;
-            var v = map.Start + (k - map.Key);
+            var v = map.Destination + (k - map.Source);
             return v;
+        }
+
+        private long SeedToLocation(long seed)
+        {
+            var soil = MapValue(seed, _maps["seed-to-soil"]);
+            var fertilizer = MapValue(soil, _maps["soil-to-fertilizer"]);
+            var water = MapValue(fertilizer, _maps["fertilizer-to-water"]);
+            var light = MapValue(water, _maps["water-to-light"]);
+            var temperature = MapValue(light, _maps["light-to-temperature"]);
+            var humidity = MapValue(temperature, _maps["temperature-to-humidity"]);
+            var location = MapValue(humidity, _maps["humidity-to-location"]);
+
+            return location;
         }
 
         private void PartOne()
         {
-            var l = _seeds.Select(s => SeedToLocation(s)).Prepend(long.MaxValue).Min();
+            var l = _seeds.Select(SeedToLocation).Min();
 
             Console.WriteLine("Day Five Part One: " + l);
         }
 
         private void PartTwo()
         {
+            var l = long.MaxValue;
+            for (var i = 0; i < _seeds.Count; i += 2)
+            {
+                var r = new Range(_seeds[i], _seeds[i + 1]);
+                var location = SeedsToLocation(r);
+                if (location < l)
+                {
+                    l = location;
+                }
+            }
 
+            Console.WriteLine("Day Five Part Two: " + l);
         }
 
-        long SeedToLocation(long s)
+        private long SeedsToLocation(Range r)
         {
-            var soil = SearchMaps(s, "seed-to-soil");
-            var fertilizer = SearchMaps(soil, "soil-to-fertilizer");
-            var water = SearchMaps(fertilizer, "fertilizer-to-water");
-            var light = SearchMaps(water, "water-to-light");
-            var temperature = SearchMaps(light, "light-to-temperature");
-            var humidity = SearchMaps(temperature, "temperature-to-humidity");
-            var location = SearchMaps(humidity, "humidity-to-location");
+            var seeds = new List<Range> { r };
+            var soil = MapValue(seeds, _maps["seed-to-soil"]);
+            var fertilizer = MapValue(soil, _maps["soil-to-fertilizer"]);
+            var water = MapValue(fertilizer, _maps["fertilizer-to-water"]);
+            var light = MapValue(water, _maps["water-to-light"]);
+            var temperature = MapValue(light, _maps["light-to-temperature"]);
+            var humidity = MapValue(temperature, _maps["temperature-to-humidity"]);
+            var location = MapValue(humidity, _maps["humidity-to-location"]);
 
-            // Console.WriteLine($"Seed {s}, soil {soil}, fertilizer {fertilizer}, water {water}, light {light}, temperature {temperature}, humidity {humidity}, location {location}.");
+            return location.Min(s => s.Start);
+        }
 
-            return location;
+        private List<Range> MapValue(List<Range> ranges, List<Map> maps)
+        {
+            var holding = ranges.Select(r => new Range(r.Start, r.Length)).ToList();
+
+            foreach (var map in maps)
+            {
+                holding = DivideRangesByMap(holding, map);
+            }
+
+            var result = new List<Range>();
+            foreach (var range in holding)
+            {
+                result.Add(new Range(MapValue(range.Start, maps), range.Length));
+            }
+
+            return result;
+        }
+
+        List<Range> DivideRangesByMap(List<Range> r, Map m, int depth = 0)
+        {
+            return r.SelectMany(x => DivideRangeByMap(x, m)).ToList();
+        }
+
+        List<Range> DivideRangeByMap(Range r, Map m, int depth = 0)
+        {
+            var result = new List<Range>();
+            // Range doesn't intersect map
+            if (r.End < m.Source || r.Start > m.SourceEnd)
+            {
+                return new List<Range> { r };
+            }
+
+            // left intersect
+            if (r.Start < m.Source)
+            {
+                return DivideRangesByMap(r.DivideAt(m.Source), m, depth + 1);
+            }
+
+            // right intersect
+            if (r.End > m.SourceEnd)
+            {
+                return DivideRangesByMap(r.DivideAt(m.SourceEnd+1), m, depth + 1);
+            }
+
+            return new List<Range> { r };
         }
 
         internal class Map
@@ -130,13 +200,58 @@ namespace Advent2023
             internal Map(List<long> src)
             {
                 if (src.Count != 3) throw new Exception("Wrong number of numbers");
-                Start = src[0];
-                Key = src[1];
+                Destination = src[0];
+                Source = src[1];
                 Length = src[2];
+                SourceEnd = Source + Length - 1;
             }
-            internal long Key { get; set; }
-            internal long Start { get; set; }
-            internal long Length { get; set; }
+
+            public override string ToString()
+            {
+                return $"[{Source}-{SourceEnd}]->[{Destination}-{Destination + Length - 1}";
+            }
+
+            internal long Destination { get; }
+            internal long Source { get; }
+            internal long SourceEnd { get; }
+            internal long Length { get; }
         }
+
+        internal class Range
+        {
+            internal Range(long start, long length)
+            {
+                Start = start;
+                Length = length;
+                End = start + length - 1;
+            }
+
+            internal long Start { get; }
+            internal long Length { get; }
+            internal long End { get; }
+
+            public override string ToString()
+            {
+                return $"[{Start}-{End}]({Length})";
+            }
+
+            internal List<Range> DivideAt(long newStart)
+            {
+                if (newStart < Start || newStart > End)
+                {
+                    return new List<Range> { this };
+                }
+
+                var l1 = newStart - Start;
+                var l2 = End - newStart + 1;
+
+                return new List<Range>
+                {
+                    new Range(Start, l1),
+                    new Range(newStart, l2)
+                };
+            }
+        }
+
     }
 }
